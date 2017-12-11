790f5d99e54ae4dcd2e65327d53f1b3338bfa7a1
Moritz Stoldt <mstoldt@gmail.com>
2017-12-11 11:43:47 +0100

    Alle Fahrräder eingefügt

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,7 @@ import { ProgrammplanungComponent } from './programmplanung/programmplanung.comp
 import { StuecklisteComponent } from './stueckliste/stueckliste.component';
 import { TestTabComponent } from './test-tab/test-tab.component';
 
-// TODO SW: Produktionsplanung + Stammdaten einfügen + Components erstellen
+// TODO SW: Produktionsplanung + Stammdaten einfügen + Components erstellengit
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/programmplanung', pathMatch: 'full'},
