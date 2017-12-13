431f8fd3d51040f5ee355cc843aabad92e6ea569
steffenwohlers <you@example.com>
2017-12-13 07:32:34 +0100

    Ändeurngen am Parameter Tab

--- src/app/parameter/parameter.component.ts
+++ src/app/parameter/parameter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LieferdatenService } from '../shared/lieferdaten.service';
+import { FahrradService } from '../shared/fahrrad.service';
+import { Fahrrad } from '../shared/fahrrad.model';
 
 @Component({
   selector: 'app-parameter',
@@ -7,10 +9,13 @@ import { LieferdatenService } from '../shared/lieferdaten.service';
   styles: []
 })
 export class ParameterComponent implements OnInit {
+  fahrrad: Fahrrad;
 
-  constructor(public lieferdatenService: LieferdatenService) { }
+  constructor(private fahrradService: FahrradService) { }
 
   ngOnInit() {
+    this.fahrrad = this.fahrradService.getFahrrad(0);
+    console.log(this.fahrrad.gabel.lieferdaten.vorlaufzeit);
   }
 
 }
