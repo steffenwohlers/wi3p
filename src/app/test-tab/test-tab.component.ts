900bf21b83a20579d7593d307a933c4fa8209e43
steffenwohlers <you@example.com>
2017-12-20 14:56:18 +0100

    test app changes

--- src/app/test-tab/test-tab.component.ts
+++ src/app/test-tab/test-tab.component.ts
@@ -14,15 +14,29 @@ export class TestTabComponent implements OnInit {
   fahrrad: Fahrrad;
   fahrrad2: Fahrrad;
 
-    constructor(private fahrradService: FahrradService, public lieferdatenService: LieferdatenService) {
+  value1 = 0;
+  value2 = 0;
+  losgroesse = this.value2 + this.value1;
+  constructor(private fahrradService: FahrradService, public lieferdatenService: LieferdatenService) {
 
-      this.fahrraeder = fahrradService.getFahrraeder();
-      this.fahrrad = this.fahrraeder[0];
-      this.fahrrad2 = this.fahrraeder[1];
-     }
+    this.fahrraeder = fahrradService.getFahrraeder();
+    this.fahrrad = this.fahrraeder[0];
+    this.fahrrad2 = this.fahrraeder[1];
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
+  }
+
+  losgroesseErreicht(): boolean {
+    let result: boolean;
+
+    if (this.losgroesse >= 500) {
+      result = true;
+    } else {
+      result = false;
     }
+    return result;
+  }
 
 }
