061cdbc0da0e91b0141d8b069b06b7e79126fae4
Moritz Stoldt <mstoldt@gmail.com>
2018-01-10 08:33:11 +0100

    Berechnung vom Rückstand auf das komplette Jahr, wenn Nachfrage Werte verändert werden

--- src/app/shared/programmplanung.model.ts
+++ src/app/shared/programmplanung.model.ts
@@ -62,7 +62,7 @@ export class Programmplanung {
                 let rest = 0;
                 for (let t = 1; t <= anzahlTage; ++t) {
                     const aktuellerTag = new Date(jahr, m, t);
-                    if ( DatumService.istArbeitstag( aktuellerTag ) ) {
+                    if (DatumService.istArbeitstag(aktuellerTag)) {
                         // tslint:disable-next-line:max-line-length
                         const abgerundet = Math.floor(dailyOutput + rest);
                         this.jahresWerte[jahr][m][t] = abgerundet;
@@ -85,7 +85,7 @@ export class Programmplanung {
     public calculateOutput() {
         const aktuellesDatum = new Date(Programmplanung.startDatum);
 
-        // Gehe durch die nächsten 52 Wochen
+        // Gehe durch die nächsten 52 Wochen für die Programmplanung
         for (let woche = 0; woche < 52; ++woche) {
             const montag = new Date(aktuellesDatum);
             let output = 0;
@@ -110,12 +110,20 @@ export class Programmplanung {
         const datum = new Date(Programmplanung.startDatum);
         for (let tage = 0; tage < 364; ++tage) {
 
+            let rueckstand;
+            if (tage === 0) {
+                rueckstand = 0;
+            } else {
+                rueckstand = this.produktionsplanung[tage - 1].rueckstand;
+            }
+
             const plannedOutput = this.getOutputPerDay(datum);
+            const needToProduce = plannedOutput + rueckstand;
             const maxOutput = this.getMaxOutput(datum);
             let real;
 
-            if (plannedOutput < maxOutput) {
-                real = plannedOutput;
+            if (needToProduce < maxOutput) {
+                real = needToProduce;
             } else {
                 real = maxOutput;
             }
@@ -125,44 +133,48 @@ export class Programmplanung {
                 this.teile,
                 plannedOutput,
                 real, // <- where magic happens
-                plannedOutput - real
+                needToProduce - real
             );
             datum.setDate(datum.getDate() + 1);
         }
     }
 
     private getMaxOutput(datum: Date): number {
-        // Unter Berücksichtigung von max. Kapazität, Lager und Lieferung den maximalen Wert pro Tag
 
-        // Der maximal am Tag zu produzierende Wert
-        const maxKapazität = 8 * 64;
+        // Wenn an dem Tag kein Arbeitstag ist, dann gebe 0 zurück
+        if (!DatumService.istArbeitstag(datum)) {
+            return 0;
+        } else {
+            // Der maximal am Tag zu produzierende Wert
+            const maxKapazität = 8 * 64;
 
-        // Das lager-Array fängt die maximalen Werte die mit dem Lager produziert werden können je Teil
-        const lager = [];
-        this.teile.forEach(teil => {
-            let maxTeil = teil.lagerbestand;
+            // Das lager-Array fängt die maximalen Werte die mit dem Lager produziert werden können je Teil
+            const lager = [];
+            this.teile.forEach(teil => {
+                let maxTeil = teil.lagerbestand;
 
-            // Wenn der Bestand im Lager unter der maxKapazität liegt, finde heraus ob das Teil rechtzeitig geliefert wäre
-            if (maxTeil < maxKapazität) {
+                // Wenn der Bestand im Lager unter der maxKapazität liegt, finde heraus ob das Teil rechtzeitig geliefert wäre
+                if (maxTeil < maxKapazität) {
 
-                // TODO: @Steffen, hier muss die Supply Chain befragt werden
+                    // TODO: @Steffen, hier muss die Supply Chain befragt werden
 
-                /*if (supplyChain.istLieferungMoeglich(datum, teil)) {
-                    // Wenn ja, dann ist ja in Wirklichkeit maxKapazität der höchst mögliche Output, da nachbestellt werden könnte
-                    maxTeil = maxKapazität;
-                }*/
-            }
+                    /*if (supplyChain.istLieferungMoeglich(datum, teil)) {
+                        // Wenn ja, dann ist ja in Wirklichkeit maxKapazität der höchst mögliche Output, da nachbestellt werden könnte
+                        maxTeil = maxKapazität;
+                    }*/
+                }
 
-            // Füge den maximalen Produktionswert für dieses Teil in das Array ein
-            lager.push(maxTeil);
-        });
+                // Füge den maximalen Produktionswert für dieses Teil in das Array ein
+                lager.push(maxTeil);
+            });
 
-        // Nehme den minimalen Wert aus dem Lager als letztendlichen Wert vom Lager - denn wenn noch 1000 Gabeln da sind,
-        // aber nur 10 Rahmen und diese auch nicht nachgeliefert werden können, können nur 10 Einheiten produziert werden
-        const maxLager = this.min(lager);
+            // Nehme den minimalen Wert aus dem Lager als letztendlichen Wert vom Lager - denn wenn noch 1000 Gabeln da sind,
+            // aber nur 10 Rahmen und diese auch nicht nachgeliefert werden können, können nur 10 Einheiten produziert werden
+            const maxLager = this.min(lager);
 
-        // Gebe diesen Wert zurück
-        return maxLager;
+            // Gebe diesen Wert zurück
+            return maxLager;
+        }
     }
 
     private min(arr): number {
@@ -184,11 +196,11 @@ export class Programmplanung {
         // Die Referenz wird überschrieben, um keine ungewollten Wechselwirkungen zu ermöglichen
         aktuellerTag = new Date(aktuellerTag);
 
-        // Wie viele Arbeitstage gibt es in diesem Monat?
+        // Wie viele Arbeitstage gibt es in dieser Woche?
         let arbeitsTageWoche = 0;
         const arbeitsTag = new Date(aktuellerTag);
         for (let tag = 0; tag < 7; ++tag) {
-            if ( DatumService.istArbeitstag( arbeitsTag ) ) {
+            if (DatumService.istArbeitstag(arbeitsTag)) {
                 arbeitsTageWoche += 1;
             }
             arbeitsTag.setDate(arbeitsTag.getDate() + 1);
@@ -197,28 +209,84 @@ export class Programmplanung {
         // Berechne benötigten Tagesoutput
         const dailyOutput = wochenDemand / arbeitsTageWoche;
 
+        // Der Rückstand vom letzten Tag wird ermittelt - wenn heute der Start ist, dann ist der Rückstand 0
+        let rueckstand = 0;
+        if (aktuellerTag.getTime() !== Programmplanung.startDatum.getTime()) {
+            const gestern = new Date(aktuellerTag);
+            gestern.setDate(gestern.getDate() - 1);
+
+            this.produktionsplanung.forEach(produktionsplanung => {
+                if (produktionsplanung.datum.getTime() === gestern.getTime()) {
+                    rueckstand = produktionsplanung.rueckstand;
+                }
+            });
+        }
+
+        // Berechne die neuen Werte für diese Woche
         let rest = 0;
         for (let tag = 0; tag < 7; ++tag) {
-            if ( DatumService.istArbeitstag( aktuellerTag ) ) {
-                const abgerundet = Math.floor(dailyOutput + rest);
-                this.updateProduktionsPlanung(aktuellerTag, abgerundet);
-                rest += dailyOutput - abgerundet;
+            if (DatumService.istArbeitstag(aktuellerTag)) {
+
+                const planned = Math.floor(dailyOutput + rest);
+                rest += dailyOutput - planned;
+
+                let real;
+                const needToProduce = planned + rueckstand;
+                const maxOutput = this.getMaxOutput(aktuellerTag);
+                if (needToProduce < maxOutput) {
+                    real = needToProduce;
+                } else {
+                    real = maxOutput;
+                }
+                rueckstand += (planned - real);
+
+                this.updateProduktionsPlanung(aktuellerTag, planned, real, rueckstand);
             } else {
-                this.updateProduktionsPlanung(aktuellerTag, 0);
+                this.updateProduktionsPlanung(aktuellerTag, 0, 0, rueckstand);
             }
             aktuellerTag.setDate(aktuellerTag.getDate() + 1);
         }
+
+        // Gehe vom ersten Tag der nächsten Woche bis zum Ende und berechne real mithilfe von Rückstand neu
+        for (let i = 0; i < this.produktionsplanung.length; ++i) {
+            if (this.produktionsplanung[i].datum.getTime() >= aktuellerTag.getTime()) {
+
+                // Nutze den letzten Rückstand vom Vortag
+                console.log(this.produktionsplanung[i - 1].datum, this.produktionsplanung[i - 1].rueckstand);
+                const letzterRueckstand = this.produktionsplanung[i - 1].rueckstand;
+
+                // Wenn der Rückstand 0 ist, muss nichts mehr aufgerechnet werden
+                if (letzterRueckstand === 0) {
+                    break;
+                } else {
+                    const real = this.produktionsplanung[i].real;
+                    const maxOutput = this.getMaxOutput(this.produktionsplanung[i].datum);
+                    const needToProduce = real + letzterRueckstand;
+
+                    let neuesReal;
+                    if (needToProduce < maxOutput) {
+                        neuesReal = needToProduce;
+                    } else {
+                        neuesReal = maxOutput;
+                    }
+
+                    this.produktionsplanung[i].real = neuesReal;
+                    this.produktionsplanung[i].rueckstand += needToProduce - neuesReal;
+                }
+            }
+        }
+
     }
 
-    private updateProduktionsPlanung(tag: Date, output: number) {
+    private updateProduktionsPlanung(tag: Date, planned: number, real: number, rueckstand: number) {
         for (let i = 0; i < this.produktionsplanung.length; ++i) {
             if (this.produktionsplanung[i].datum.getTime() === tag.getTime()) {
                 this.produktionsplanung[i] = new Produktionsplanung(
                     new Date(tag),
                     this.teile,
-                    output,
-                    0,
-                    0
+                    planned,
+                    real,
+                    rueckstand
                 );
                 return;
             }
